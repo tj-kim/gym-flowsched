994afb5d09e613a94aa654eb9ea5fd3a9105e294
ChelseaZhang90 <8079703+ChelseaZhang90@users.noreply.github.com>
2019-03-14 23:50:49 -0700

    Ready to run

--- gym_flowsched/envs/flowsched_env.py
+++ gym_flowsched/envs/flowsched_env.py
@@ -1,4 +1,5 @@
 import sys
+import numpy as np
 from gym.envs.toy_text import discrete
 
 class FlowSchedEnv(discrete.DiscreteEnv):
@@ -34,67 +35,77 @@ class FlowSchedEnv(discrete.DiscreteEnv):
     def __init__(self):
         nS = 20
         nA = 3
-        nF = 40 
-        min_last = 4 * np_random
+        isd = [1/nS for x in range(nS)]
+        self.nF = 10 
+        self.min_last = 1 * np.random.random()
+        self.rm_size = []
+        self.flow_time_link = 0
 
-        wt = [[np_random for i in range(nS)] for j in range(nA)]
-        bandwidth_cap = [i+1 for i in range(nS)]
-        rate = np.matmul(wt,bandwidth_cap)
+        wt = [[np.random.random() for i in range(nS)] for j in range(nA)]
+        self.bandwidth_cap = [i+1 for i in range(nS)]
+        self.rate = np.matmul(wt,np.diag(self.bandwidth_cap))
+        # dimension: nA x nS
 
         P = {s: {a: [] for a in range(nA)} for s in range(nS)}
         for s in range(nS):
             for a in range(nA):
                 for next_s in range(nS):
-                    P[s][a].append((1/nS, next_s, rate[s]))
-        self.is_flows_completed = False
-        self.remain_traffic = 0
+                    P[s][a].append((1/nS, next_s, self.rate[a][s]))
+
         self.num_flows = 0
+        discrete.DiscreteEnv.__init__(
+            self, nS, nA, P, isd)
 
 
     def render(self, mode='human'):
-        pass
+        print(self.flow_time_link)
 
-    def _get_flow_time(rm_size, flow_time_link, bandwidth_cap, rate):
+    def _get_flow_time(self, rm_size, flow_time_link, bandwidth_cap, rate):
+        """
+        rm_size: a vector with a dynamic dimention ranging from 0 to nF
+        flow_time_link: a constant
+        rate = rate[s][a], a constant
+        """
         if rm_size != []:
             i_shortest_flow = np.argmin(rm_size)
         
         time_out = 0
         while time_out < 1 and rm_size != []:
-            rate_per_flow = rate / num_flows # Needs modification for multiple links 
+            rate_per_flow = rate / self.num_flows # Needs modification for multiple links 
             if min(rm_size) > rate_per_flow * (1-time_out):
-                flow_time_link += (1-time_out) * num_flows
+                flow_time_link += (1-time_out) * self.num_flows
                 rm_size = [x - rate_per_flow * (1-time_out) for x in rm_size]
             else:
                 # The following two lines need modification if rate_per_flow is different over flows
                 time_shortest_flow = min(rm_size) / rate_per_flow
                 time_out += time_shortest_flow
-                flow_time_link += time_shortest_flow * num_flows
+                flow_time_link += time_shortest_flow * self.num_flows
                 rm_size = [x - min(rm_size) for x in rm_size]
-                for i in range(size(rm_size)):
+                for i in range(np.size(rm_size)):
                     if rm_size[i] <= 0:
-                        num_flows -= 1
+                        self.num_flows -= 1
                         rm_size.remove(rm_size[i])
 
         return rm_size, flow_time_link
 
     def step(self, a):
-        if self.num_flows < nF:
-            self.newflow_size = nS * min_last
+        if self.num_flows < self.nF:
+            self.newflow_size = self.nS * self.min_last
             self.rm_size.append(self.newflow_size)
             self.num_flows += 1
-            self.remain_traffic += self.newflow_size
 
         transitions = self.P[self.s][a]
-        i = categorical_sample([t[0] for t in transitions], self.np_random)
+        i = discrete.categorical_sample([t[0] for t in transitions], self.np_random)
         prob, newstate, reward = transitions[i]
         self.s = newstate
 
-        self.rm_size, self.flow_time_link = _get_flow_time(self.rm_size, self.flow_time_link, bandwidth_cap, rate)
+        self.rm_size, self.flow_time_link = self._get_flow_time(self.rm_size, self.flow_time_link, self.bandwidth_cap, self.rate[a][self.s])
  
 
-        if rm_size == [] and self.num_flows >= nF:
+        if self.rm_size == [] and self.num_flows >= self.nF:
             done = True
         else:
             done = False
-        return (newstate, reward, done, flow_time_link, prob)
+        #return (newstate, reward, done, self.flow_time_link, prob)
+        return (newstate, reward, done, prob)
 
